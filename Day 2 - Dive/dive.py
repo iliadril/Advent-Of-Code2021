a67c5888e55061116b11b38566cac3f3eb894e50
iliadril <xiliadril@gmail.com>
2021-12-03 11:32:37 +0100

    Day 2

--- Day 2 - Dive/dive.py
+++ Day 2 - Dive/dive.py
@@ -2,8 +2,22 @@ with open("./input", "r") as f:
     data = [line.split(" ") for line in f]
     data = [(comm, int(n)) for comm, n in data]
 
+# ---------------------------------- PART ONE ----------------------------------
 horizontal = sum(x[1] for x in data if x[0] == 'forward')
 vertical = sum(x[1] for x in data if x[0] == 'down')
 vertical -= sum(x[1] for x in data if x[0] == 'up')
 
-print(f'Horizontal distance: {horizontal}\nVertical distance: {vertical}\nProduct: {horizontal*vertical}')
+print(f'Horizontal distance: {horizontal}\nVertical distance: {vertical}\nProduct: {horizontal*vertical}\n')
+
+# ---------------------------------- PART TWO ----------------------------------
+horizontal, depth, aim = 0, 0, 0
+for comm, n in data:
+    if comm == 'down':
+        aim += n
+    elif comm == 'up':
+        aim -= n
+    else:
+        horizontal += n
+        depth += n*aim
+
+print(f'Horizontal distance: {horizontal}\nVertical distance: {depth}\nProduct: {horizontal*depth}')
