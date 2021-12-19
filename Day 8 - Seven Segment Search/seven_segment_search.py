f00a7aaaf11f21da1f00ab136a9280f85dc20fff
iliadril <xiliadril@gmail.com>
2021-12-19 03:23:32 +0100

    Day 8

--- Day 8 - Seven Segment Search/seven_segment_search.py
+++ Day 8 - Seven Segment Search/seven_segment_search.py
@@ -1,61 +1,42 @@
-from collections import defaultdict
-
-with open("./test_input", "r") as f:
+with open("./input", "r") as f:
     data = []
     for line in f:
         nums, out = line.split(' | ')
-        data += [{'i': nums.split(), 'o': out.split()}]
+        data += [(nums.split(), out.split())]
 
 # ---------------------------------- PART ONE ----------------------------------
-print(f"Part One answer: {sum([1 for line in data for val in line['o'] if len(val) in (2, 3, 4, 7)])}")
+print(f"Part One answer: {sum([1 for line in data for val in line[1] if len(val) in (2, 3, 4, 7)])}")
 
 # ---------------------------------- PART TWO ----------------------------------
-display_dict = {0: ['a', 'b', 'c', 'e', 'f', 'g'],
-                1: ['c', 'f'],
-                2: ['a', 'c', 'd', 'e', 'g'],
-                3: ['a', 'c', 'd', 'f', 'g'],
-                4: ['b', 'c', 'd', 'f'],
-                5: ['a', 'b', 'd', 'f', 'g'],
-                6: ['a', 'b', 'd', 'e', 'f', 'g'],
-                7: ['a', 'c', 'f'],
-                8: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
-                9: ['a', 'b', 'c', 'd', 'f', 'g']}
+out_sum = 0
+for inp, outp in data:
+    num_match = [set()] * 10
+    num_match[1] = list(set(val) for val in inp if len(val) == 2)[0]
+    num_match[4] = list(set(val) for val in inp if len(val) == 4)[0]
+    num_match[7] = list(set(val) for val in inp if len(val) == 3)[0]
+    num_match[8] = list(set(val) for val in inp if len(val) == 7)[0]
 
+    fiveters = (set(val) for val in inp if len(val) == 5)
+    for num in fiveters:
+        if num_match[1].issubset(num):
+            num_match[3] = num
+        elif len(num & num_match[4]) == 3:
+            num_match[5] = num
+        else:
+            num_match[2] = num
 
-def decode_digit(l, d):
-    for sequence in l:
-        match_dict = {'a': [],
-                      'b': [],
-                      'c': [],
-                      'd': [],
-                      'e': [],
-                      'f': [],
-                      'g': []}
+    sixters = (set(val) for val in inp if len(val) == 6)
+    for num in sixters:
+        if num_match[4].issubset(num):
+            num_match[9] = num
+        elif num_match[5].issubset(num):
+            num_match[6] = num
+        else:
+            num_match[0] = num
 
-        # counting occurences
-        occ_dict = defaultdict(int)
-        for num in sequence['i']:
-            for ch in num:
-                occ_dict[ch] += 1
+    s = 0
+    for o in outp:
+        s = s * 10 + num_match.index(set(o))
+    out_sum += s
 
-        match_dict['e'] = [ch for ch, val in occ_dict.items() if val == 4]
-        match_dict['b'] = [ch for ch, val in occ_dict.items() if val == 6]
-        match_dict['f'] = [ch for ch, val in occ_dict.items() if val == 9]
-
-        # match which segments could be which
-        for num in sequence['i']:
-            for val in d.values():
-                if len(num) == len(val) and len(num) in (2, 3, 4, 7):
-                    if len(num) == 2:
-                        print(list(num))
-                        print(match_dict.values())
-                    for ch in val:
-                        if ch not in ('b', 'e', 'f'):
-                            match_dict[ch] = [v for v in val if v not in (*match_dict['e'], *match_dict['b'], *match_dict['f'])]
-
-        for num in sequence['i']:
-            pass
-        print(match_dict)
-
-
-decode_digit(data, display_dict)
+print(f"Part Two answer: {out_sum}")
